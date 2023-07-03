cafac0c626418c7de5397e9407ab13d3b0890836
dobak-david <81691825+dobak-david@users.noreply.github.com>
2023-07-03 20:38:56 +0200

    delete id from the table

--- car_rental/resources/views/cars/index.blade.php
+++ car_rental/resources/views/cars/index.blade.php
@@ -49,7 +49,7 @@
                         <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                             <th scope="row"
                                 class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                {{ $car->marka }}{{ $car->id }}
+                                {{ $car->marka }}
                             </th>
                             <td class="px-6 py-4">
                                 {{ $car->tipus }}
